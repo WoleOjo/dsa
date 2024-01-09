c9da61f4e64dd2c3ae0c293e5eceb99e6bfeb3c3
Wole Ojo <woleojo@Woles-MacBook-Air.local>
2024-01-09 10:26:24 +0100

    binary search

--- Sorting_Algorithms/binary_search.py
+++ Sorting_Algorithms/binary_search.py
@@ -1,20 +1,4 @@
 import math
-#Works for sorted list
-"""
-0. check for empty list
-	return -1 if empty
-1.set two bonds: low and high
-2.set mid as the mid point between low and high
-3.initial low and high is beginning and end of array idex
-4.while high not equal to
-	if  mid element == query
-		return mid
-	if mid element is greater than query, set new high to mid - 1
-	if mid element is less than query, set new low to mid + 1
-
-5. return not found
-"""
-
 def bin_s (list,query):
 	low = 0
 	high = len(list) - 1
